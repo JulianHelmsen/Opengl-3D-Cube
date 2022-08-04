bf9ce53ba0c486616b15445cbc819aff3c1db726
LordBacki <102655974+LordBacki@users.noreply.github.com>
2022-08-04 22:46:19 +0200

    WINDOW!

--- 3d-cube/src/Entrypoint.cpp
+++ 3d-cube/src/Entrypoint.cpp
@@ -1,9 +1,23 @@
 #include <stdio.h>
+#include <GLFW/glfw3.h>
 
 int main(const int argc, const char** argv) {
 	printf("Hello World\n");
 
+	if(!glfwInit()) return -1;
 
-	return 0;
+	GLFWwindow* window = glfwCreateWindow(1920, 1080, "BigJ", nullptr, nullptr);
+
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 }
