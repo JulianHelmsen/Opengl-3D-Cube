c1a4a790891a33dce317b3e7a94b96404953d062
LordBacki <102655974+LordBacki@users.noreply.github.com>
2022-08-07 16:11:03 +0200

    Implemented indexBuffer, draw rectangle

--- 3d-cube/src/app.cpp
+++ 3d-cube/src/app.cpp
@@ -4,21 +4,26 @@
 
 GLuint vertexArray;
 GLuint vertexBuffer;
+GLuint indexBuffer;
 GLuint program;
 
 void onRender() {
 	glUseProgram(program);
 	glBindVertexArray(vertexArray);
-	glDrawArrays(GL_TRIANGLE_FAN, 0, 3);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 }
 
 void onInit() {
-	GLfloat vertices[] = {0.5f, 0.5f, 0.0f, -0.5f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f};
+	GLfloat vertices[] = {0.5f, 0.5f, 0.0f, -0.5f, 0.5f, 0.0f, 0.5f, -0.5f, 0.0f, -0.5f, -0.5f, 0.0f};
+	GLuint connectionOrder[] = { 1, 0, 2, 1, 2, 3 };
 	glGenVertexArrays(1, &vertexArray);
 	glBindVertexArray(vertexArray);
-	glGenBuffers(100, &vertexBuffer);
+	glGenBuffers(1, &vertexBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glGenBuffers(1, &indexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(connectionOrder), connectionOrder, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, nullptr);
 
