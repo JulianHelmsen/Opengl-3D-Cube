539ff481daa8a6c66e70ca5d299a389860535552
JulianHelmsen <juliandominikhelmsen@gmail.com>
2022-08-11 23:10:39 +0200

    set window

--- 3d-cube/src/input.cpp
+++ 3d-cube/src/input.cpp
@@ -1,12 +1,18 @@
 #include "input.h"
 
+static int s_prev_mouse_x = -1;
+static int s_prev_mouse_y = -1;
+
+static GLFWwindow* s_window;
+
 float input::getDeltaXPos() {
+
 }
 float input::getDeltaYPos() {
 
 }
 void input::setWindow(GLFWwindow* windowPtr) {
-
+	s_window = windowPtr;
 }
 bool input::isButtonPressed() {
 
